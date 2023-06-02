d0bfe125ff640bdcd0e703a72954e042cc1388f8
jakob <j.d.bichler@student.tudelft.nl>
2023-06-02 14:59:53 +0200

    added state_machine launch and first parts

---
+++ state_machine/src/state_machine.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+import actionlib
+
+from human_interaction.msg import ConversationAction, ConversationGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+
+# define state HelpNeeded
+class Conversation(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=["object_specified", "object_not_specified"],
+                             output_keys=["object_id"])
+        
+
+        self.client = actionlib.SimpleActionClient('conversation', ConversationAction)
+        rospy.loginfo("Waiting for 'conversation' action server...")
+        self.client.wait_for_server()
+        self.goal = ConversationGoal()
+
+
+    def execute(self, userdata):
+        rospy.loginfo("Executing state Conversation")
+ 
+        self.goal.conv_type = "give_mission"  # Change this based on your requirements
+        rospy.loginfo(f"Started conversation \"{self.goal.conv_type}\"")
+        self.client.send_goal(self.goal)
+        self.client.wait_for_result(rospy.Duration(60))
+        result = self.client.get_result()
+        
+        if result.answer:  
+            # Action completed successfully
+            userdata.object_id = result.answer
+            return 'object_specified'
+        else:
+            # Action did not complete within the timeout
+            return 'object_not_specified'
+
+
+class Approach(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=["goal_reached", "failed_goal_reached"],
+                             input_keys=["object_id"])
+
+        
+        self.client = actionlib.SimpleActionClient('motion_control', MoveBaseAction)
+        rospy.loginfo("Waiting for 'motion_control' action server...")
+        self.client.wait_for_server()
+        self.goal = MoveBaseGoal()
+
+
+    def execute(self, userdata):
+        rospy.loginfo("Executing state motion_control")
+ 
+        # if userdata.object_id == "orange":
+        #     print("Object ID: ", userdata.object_id)
+        #     return 'goal_reached'
+        
+        # else:
+        #     print("Object ID: ", userdata.object_id)
+        #     return 'failed_goal_reached'
+        
+        self.goal.target_pose.pose.position.x = 80
+        self.goal.target_pose.pose.position.y = 80
+
+        self.client.send_goal(self.goal)
+        self.client.wait_for_result(rospy.Duration(60))
+        result = self.client.get_result()
+        
+        if result:  
+            # Action completed successfully
+            rospy.loginfo("Goal reached")
+            return 'goal_reached'
+        else:
+            # Action did not complete within the timeout
+            return 'failed_goal_reached'
+        
+
+class Mapping(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=["done", "not_done"])
+
+        self.round_counter = 0
+        self.client = actionlib.SimpleActionClient('motion_control', MoveBaseAction)
+        rospy.loginfo("Waiting for 'motion_control' action server...")
+        self.client.wait_for_server()
+        self.goal = MoveBaseGoal()
+
+
+    def execute(self, userdata):
+        rospy.loginfo(f"Executing state mapping for the {self.round_counter} time")
+
+        if self.round_counter == 10:
+            rospy.loginfo("Mapping done")
+            return 'done'
+        
+        else:
+            self.round_counter += 1
+            rospy.loginfo(self.round_counter)
+            return 'not_done'
+
+
+if __name__ == '__main__':
+    rospy.init_node('smach_example_state_machine')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=["finished", "failed"])
+
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add("MAPPING", Mapping(), 
+                        transitions={"done": "CONVERSATION", 
+                                    "not_done": "MAPPING"},
+                        remapping={"item_id": "item_id"})
+        
+
+        smach.StateMachine.add("CONVERSATION", Conversation(), 
+                               transitions={"object_specified": "APPROACH", 
+                                            "object_not_specified": "failed"},
+                               remapping={"item_id": "item_id"})
+
+        smach.StateMachine.add("APPROACH", Approach(), 
+                        transitions={"goal_reached": "finished", 
+                                    "failed_goal_reached": "failed"},
+                        remapping={"item_id": "item_id"})
+        
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis.start()
+
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+    rospy.spin()
+    sis.stop()
+ 
